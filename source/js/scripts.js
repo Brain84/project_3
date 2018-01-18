97bf4d7416aaa1343f2df1e6937aeb7aa4d37473
Pawel Tatar <pawel.tatar.email@gmail.com>
2018-01-19 00:37:57 +0100

    website_3

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,27 +1,23 @@
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+
 //open nav list
-(function(){
-	'use strict';
-	$(function(){
-		$("#burger-check").click(function(){
-			$(".header_nav_list").toggleClass("open-list");
-		});
-	});
-})();
+$("#burger-check").click(function(){
+	$(".header_nav_list").toggleClass("open-list");
+});
 
 //scroll to a section
-$(function() {
-	$('a[href*="#"]:not([href="#"])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+$('a[href*="#"]:not([href="#"])').click(function() {
+	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 
-			var target = $(this.hash);
+		var target = $(this.hash);
 
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: target.offset().top
-				}, 1000);
-				return false;
-			}
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		if (target.length) {
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 1000);
+			return false;
 		}
-	});
+	}
 });
